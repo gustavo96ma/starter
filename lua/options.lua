abea1163a3713e3adb2f0f0c0d062b85fdeea5ce
gustavo96ma <gustavo96ma@gmail.com>
2024-09-11 16:04:19 -0300

    sync

--- lua/options.lua
+++ lua/options.lua
@@ -5,3 +5,16 @@ require "nvchad.options"
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
 vim.opt.relativenumber = true
+
+require("markview").setup({
+    modes = { "n", "i", "no", "c" },
+    hybrid_modes = { "i" },
+
+    -- This is nice to have
+    callbacks = {
+        on_enable = function (_, win)
+            vim.wo[win].conceallevel = 2;
+            vim.wo[win].concealcursor = "nc";
+        end
+    }
+})
